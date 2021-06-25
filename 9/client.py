c36509bf897bb8b909f93d8d842ff1a90e1a0ebe
ratnadeep <ratnadeeppandu9@gmail.com>
2021-06-25 11:25:48 +0530

    improvised

--- 9/client.py
+++ 9/client.py
@@ -1,7 +1,13 @@
 from socket import *
-print('Hey this client')
+print('Hey this is client')
 s=socket(AF_INET,SOCK_STREAM)
 s.connect((gethostname(),1234))
 while True:
-    print(s.recv(1024).decode('utf-8'))
-    break
+    try:
+        k=input('enter msg')
+        s.send(bytes(k,'utf-8'))
+        print(s.recv(1024).decode('utf-8')+'- from server')
+    except KeyboardInterrupt:
+        print()
+        print('exiting -----')
+        break
--- 9/server.py
+++ 9/server.py
@@ -2,9 +2,20 @@ from socket import *
 s=socket(AF_INET,SOCK_STREAM)
 s.bind((gethostname(),1234))
 s.listen(10)
+add=tuple()
 while True:
-    cli,add=s.accept()
     if add:
-        print(f'connection is established with {add}')
-        cli.send(bytes('hey i am server','utf-8'))
-        break
+        try:
+            k=input('enter msg')
+            cli.send(bytes(k,'utf-8'))
+            print(cli.recv(1024).decode('utf-8')+'- from client')
+        except KeyboardInterrupt:
+            print()
+            print('exiting ------')
+            break
+    else:
+        try:
+            cli,add=s.accept()
+            print(f'connection is established with {add}')
+        except ConnectionError as e:
+            print(e)
